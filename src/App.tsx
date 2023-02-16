f6d717b0bb165e4bf3b1c4d9c3c92dbe1746c2f5
starcjs7 <starcjs7@gmail.com>
2023-02-16 23:21:28 +0900

    ga4 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
 import styled from 'styled-components'
+import ReactGA from "react-ga4";
 import Router from './Router';
 
 
 const App = () => {
+  useEffect(() => {
+    if (!window.location.href.includes("localhost")) {
+      ReactGA.initialize(import.meta.env.VITE_GA_ID);
+      ReactGA.send("pageview");
+    }
+  }, [])
+  
   return (
     <AppContainer>
       <Router />
